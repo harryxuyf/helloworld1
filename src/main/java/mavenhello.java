69a922cb36df64b39ce9b99791f8a3392db197ee
harry <387551654@qq.com>
2020-06-23 16:03:32 +0800

    aaa

--- src/main/java/mavenhello.java
+++ src/main/java/mavenhello.java
@@ -1,6 +1,6 @@
 public class mavenhello {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello2");
+        System.out.println("hello5");
     }
 }
